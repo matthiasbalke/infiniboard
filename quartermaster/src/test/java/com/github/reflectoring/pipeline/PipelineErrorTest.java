3d75a2c18843c877f5de6e8a8753ac9d83f81691
Matthias Balke <matthias.balke@googlemail.com>
2016-07-08 11:46:43 +0200

    added failing test for jenkins pipeline tests

---
+++ quartermaster/src/test/java/com/github/reflectoring/pipeline/PipelineErrorTest.java
@@ -0,0 +1,13 @@
+package com.github.reflectoring.pipeline;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PipelineErrorTest {
+
+    @Test
+    public void shouldFail() {
+        fail("This test fails for jenkins pipeline testing purposes.");
+    }
+}
